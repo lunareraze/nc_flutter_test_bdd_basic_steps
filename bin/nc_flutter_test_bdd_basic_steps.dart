82977d32f9fa1fc167e68521e7702e830fda4339
lunareraze <sapitridina88@gmail.com>
2024-02-28 17:45:24 +0700

    chore: initial commit

---
+++ bin/nc_flutter_test_bdd_basic_steps.dart
@@ -0,0 +1,5 @@
+import 'package:nc_flutter_test_bdd_basic_steps/nc_flutter_test_bdd_basic_steps.dart' as nc_flutter_test_bdd_basic_steps;
+
+void main(List<String> arguments) {
+  print('Hello world: ${nc_flutter_test_bdd_basic_steps.calculate()}!');
+}
---
+++ test/nc_flutter_test_bdd_basic_steps_test.dart
@@ -0,0 +1,8 @@
+import 'package:nc_flutter_test_bdd_basic_steps/nc_flutter_test_bdd_basic_steps.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
